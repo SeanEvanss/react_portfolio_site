f7adcf6fecf509b975368ba6f006c47dd28ccb99
Sean Tang <1996sean@gmail.com>
2021-09-21 01:11:16 +0800

    Deleted unnecessary generated files and created placeholder landing page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hullo this is a work in progress. I promise I'll update this soon
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
