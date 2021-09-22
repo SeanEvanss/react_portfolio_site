3e454667179890e32c17b512dad8cfdab4dd1cee
Sean Tang <1996sean@gmail.com>
2021-09-22 23:21:13 +0800

    Added 'About' component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-//import './App.css';
+import './index.css';
+import React from 'react';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hullo this is a work in progress. I promise I'll update this soon
-        </p>
-      </header>
-    </div>
-  );
+    <main className="text-gray-500 bg-gray-700 body-font">     
+      <Navbar/>
+      <About/>      
+      <Projects/>
+      <Skills/>
+      <Contact/>      
+    </main>
+    );
 }
-
-export default App;
