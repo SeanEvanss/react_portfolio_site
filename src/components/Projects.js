ceba809f078ea0d05471a221fcdf687cdb153207
Sean Tang <1996sean@gmail.com>
2021-10-12 01:18:41 +0800

    Updated the project component. Will need to optimise gif size in future.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import vesp_gif from './resources/vespertine.gif'
+import { projects } from './data';
+import vesp from "./resources/vespertine.gif"
 import { CodeIcon } from "@heroicons/react/solid";
 
 
@@ -9,19 +10,24 @@ export default function Projects() {
             <div className="container px-20 py-20 mx-auto">
                 <div className="flex flex-col w-full mb-20">
                     <div className="text-center mb-20">
-                        <CodeIcon className="w-20 inline-block"/>
-                        <h1 className="text-3xl font-medium text-white-200"> 
+                        <CodeIcon className="w-20 inline-block" />
+                        <h1 className="text-3xl font-medium text-white-200">
                             Projects I've worked on
                         </h1>
                         <p className="mt-10 text-center">
-                            I've worked on both web based as well as mobile applications<br/>
+                            I've worked on both web based as well as mobile applications<br />
                             I've developed apps for Android, iOS, web. Additioally I have also written several cool standalone projects.
                         </p>
                     </div>
-                    
-                    <img src={vesp_gif} alt="loading..." />
+                    <div className="flex flex-wrap m-4">
+                        {projects.map((project) => (
+                            <a href={project.link} key={project.key} className="sm:w-1/2 w-100 p-4">
+                                <img alt="loading..." src={project.image_link}/>
+                            </a>
+                        ))}
+                    </div>
                 </div>
-            </div>            
+            </div>
         </section>
     )
 }
--- src/components/data.js
+++ src/components/data.js
@@ -1,2 +1,29 @@
-export const skills =["C/C++", "Java" ,"ReactJS", "Android Studio", "Flutter", 
-"Python", "Tensroflow/Keras", "Unity"];
+import vesp from "./resources/vespertine.gif"
+import cursing from "./resources/cursing.gif"
+
+export const skills = [
+    "C/C++", "Java",
+    "ReactJS", "Android Studio",
+    "Flutter",
+    "Python", "Tensroflow/Keras", "Unity"
+];
+
+export const projects = [
+    {
+        title: "Vespertine",
+        subtitle: "Android",
+        description: "A simple Android application consuming data.gov and Google API. Focused on nightlife functions such as displayng all neray taxis locations as well as parking lots left at all malls and public carparks",
+        image_link: vesp,
+        link: "https://github.com/SeanEvanss/Vespertine",
+        key:0
+    },
+    {
+        title: "Cursing detection",
+        subtitle: "Python, Mediapipe",
+        description: "A Python application taking advantage of the Mediapipe library. Can detect when a certain offensive gesture is made.",
+        image_link: cursing,
+        link: "https://github.com/SeanEvanss/Vespertine",
+        key:1
+    }
+];
+
