866e0ea11722a278dd16eacf1f263d252e192eab
Sean Tang <1996sean@gmail.com>
2021-10-24 02:39:17 +0800

    Improved scaling for 'projects' component and removed old files

--- src/components/data.js
+++ src/components/data.js
@@ -1,6 +1,6 @@
 import vesp from "./resources/vespertine.gif"
 import cursing from "./resources/cursing.gif"
-import scrapper from "./resources/scrapper.gif"
+import scrapper from "./resources/new_scraper.gif"
 import flappy from "./resources/flappy.gif"
 
 export const skills = [
@@ -14,18 +14,10 @@ export const projects = [
     {
         title: "Vespertine",
         subtitle: "Android Studio, Java",
-        description: "An Android application consuming data.gov and Google API. Focused on nightlife functions such as displayng all neray taxis locations as well as parking lots left at all malls and public carparks",
+        description: "An Android application consuming data.gov and Google API. Focused on nightlife functions such as displayng all nearby taxis locations as well as parking lots left",
         image_link: vesp,
         link: "https://github.com/SeanEvanss/Vespertine",
         key:0
-    },
-    {
-        title: "Cursing detection",
-        subtitle: "Python, Mediapipe",
-        description: "A Simple Python application making use of the Mediapipe library. The script is able to detect when a certain offensive gesture is made.",
-        image_link: cursing,
-        link: "https://github.com/SeanEvanss/Cursing-finger-detection",
-        key:1
     },
     {
         title: "Web scrapper",
@@ -33,7 +25,15 @@ export const projects = [
         description: "A web scapper script I wrote using NodeJS that will be hooked into a database (eventually). Currently WIP.",
         image_link: scrapper,
         link: "https://github.com/SeanEvanss/nodejs_web_scrapper",
-        key:3
+        key:1
+    },
+    {
+        title: "Cursing detection",
+        subtitle: "Python, Mediapipe",
+        description: "A Simple Python application making use of the Mediapipe library. The script is able to detect when a certain offensive gesture is made.",
+        image_link: cursing,
+        link: "https://github.com/SeanEvanss/Cursing-finger-detection",
+        key:2
     },
     {
         title: "NEAT Flappy bird",
@@ -41,7 +41,10 @@ export const projects = [
         description: "A python script showing how a neural evolutionary network (NEAT) can be applied. We demonstrate the utility of the NEAT library with flappy bird created using pygame.",
         image_link: flappy,
         link: "https://github.com/SeanEvanss/flappy_bird_NEAT",
-        key:4
+        key:3
     },
+    
+    
+    
 ];
 
