ebf72559ccc6dc5642f1e95bc4b74e2eea1f01fc
Sean Tang <1996sean@gmail.com>
2021-11-04 22:15:24 +0800

    Added actual API request functionality for dev enviroment

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,41 @@
 import React, { useState } from "react";
 
-
 export default function Contact() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
 
-    //on form submission
+
+    //For anyone wondering why I had a discrete function for onNameUpdate,
+    //it's because I wanted to show the diff between inline and discrete fucntions to do the same thing.        
     /**
     @param {Event} e
     */
     const onNameUpdate = (e) => {
         setName(e.target.value);
-        console.log(name)
+    }
+
+    //on form submission
+    const handleSubmit = (e) => {
+        //Prevents the browser from executing the default action of the form (in this case, reloading the page)
+        e.preventDefault();
+        //Yes, I will change this to the actual API endpoint during production.
+        const URL = "http://localhost:8080/users"
+        const requestOptions = {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+                'apikey': process.env.REACT_APP_API_KEY,
+            },
+            body: JSON.stringify({ name, email, message })
+        };        
+        //The actual API request based on our supplied URL and request options
+        fetch(URL, requestOptions)
+            .then(response => alert("Message sent! I will reach out to you ASAP"))
+            .catch(error => console.log("Form submission error: ", error));
+        //Resets our form upon submission
+        e.target.reset();
     }
 
     return (
@@ -20,41 +43,42 @@ export default function Contact() {
             <div className="container mx-auto px-5 py-10 flex justify-center">
                 <form
                     className="lg:w-2/3 md:w-1/2 flex flex-col w-full md:py-8 mt-8 md:mt-0 items-center"
-                    title="form">
+                    title="form"
+                    onSubmit={handleSubmit}>
                     <h2 className="text-white sm:text-4xl text-3xl mb-5 font-medium title-font" >
                         Contact me
                     </h2>
                     <div className="flex flex-col w-full justify-center">
                         <h3 className="text-white md:mr-5 leading-7">
-                            Name: 
+                            Name:
                         </h3>
                         <input type="text"
                             id="name"
                             name="name"
                             className="w-full text-gray-800 mb-4 rounded border-2 border-yellow-500"
-                            onChange={onNameUpdate}/>
+                            onChange={onNameUpdate} />
 
                     </div>
                     <div className="flex flex-col w-full justify-center">
                         <h3 className="text-white md:mr-5 leading-7">
-                            Email: 
+                            Email:
                         </h3>
                         <input type="text"
                             id="name"
                             name="name"
                             className="w-full text-gray-800 mb-4 rounded border-2 border-yellow-500"
-                            onChange={(e)=>{setEmail(e.target.value)}}/>
+                            onChange={(e) => { setEmail(e.target.value) }} />
 
                     </div>
                     <div className="flex flex-col w-full justify-center">
                         <h3 className="text-white md:mr-5 leading-7">
-                            Name: 
+                            Message:
                         </h3>
                         <textarea type="text"
-                            id="name"
-                            name="name"
+                            id="message"
+                            name="message"
                             className=" h-32 w-full text-gray-800 mb-4 rounded border-2 border-yellow-500"
-                            onChange={(e)=>{setMessage(e.target.value)}}/>
+                            onChange={(e) => { setMessage(e.target.value) }} />
 
                     </div>
                     <button type="submit" className="py-1 px-3 rounded hover:bg-gray-900 hover:text-white focus:outline-none">
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react'
+import React from 'react'
 import "./styles/Landing.css"
 import background from './resources/background.jpeg'
 import { ArrowDownIcon } from '@heroicons/react/solid';
