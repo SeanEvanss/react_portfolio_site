93cfafa0cd8c1a772d0c498b9c061741d028849a
Sean Tang <1996sean@gmail.com>
2022-01-11 02:04:21 +0800

    Added transition animation between Routes

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import Navbar from './components/Navbar';
 import MainPage from './MainPage';
 import Feed from './components/Feed';
 import "./App.css"
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
+
+import {Route, Routes, useLocation } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <html>
-        <main className="text-gray-400 bg-custom_gray body-font">
-          <Navbar />
-          <Routes>
-            <Route path="/react_portfolio_site/" element={<MainPage />} />
-            <Route path="/react_portfolio_site/project_feed/" element={<Feed/>} />
-          </Routes>
-        </main>
-      </html>
-    </BrowserRouter>
 
+  const location = useLocation();
+  return (
+
+    <main className="text-gray-400 bg-custom_gray body-font">
+        <Navbar />
+        <TransitionGroup>
+          <CSSTransition key={location.pathname} classNames="fade" timeout={200}>
+            <Routes location={location}>
+              <Route path="/react_portfolio_site/" element={<MainPage />} />
+              <Route path="/react_portfolio_site/project_feed/" element={<Feed />} />
+            </Routes>
+          </CSSTransition>
+        </TransitionGroup>
+    </main>
   );
 }
