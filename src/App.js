80eef8092843657efb6be5f7c1b3eb0bd87d4ca7
Sean Tang <1996sean@gmail.com>
2022-01-13 23:26:36 +0800

    Added scroll-to-element functionality in navbar

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ import {Route, Routes, useLocation } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 export default function App() {
-
+  
   const location = useLocation();
   return (
 
     <main className="text-gray-400 bg-custom_gray body-font">
         <Navbar />
         <TransitionGroup>
-          <CSSTransition key={location.pathname} classNames="fade" timeout={200}>
+          <CSSTransition key={location.pathname} classNames="alert" timeout={200}>
             <Routes location={location}>
               <Route path="/react_portfolio_site/" element={<MainPage />} />
               <Route path="/react_portfolio_site/project_feed/" element={<Feed />} />
